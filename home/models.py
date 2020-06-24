feefd9ef16d55fd84f939eb4725ee1dd865f5333
Muhammad Sulaiman <mksuleiman03@gmail.com>
2020-06-24 15:56:16 +0100

    extended cta to homepage

--- home/models.py
+++ home/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
 
 
 class HomePage(Page):
@@ -32,12 +35,22 @@ class HomePage(Page):
 
     body = RichTextField(blank=True)
 
+    content = StreamField(
+        [
+            ("cards", blocks.CardBlock()),
+            ("cta", blocks.CTABlock()),
+        ],
+        null=True,
+        blank=True,
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
         FieldPanel("banner_subtitle"),
         ImageChooserPanel("banner_image"),
         PageChooserPanel("banner_cta"),
         FieldPanel('body', classname="full"),
+        StreamFieldPanel("content"),
     ]
 
     class Meta:

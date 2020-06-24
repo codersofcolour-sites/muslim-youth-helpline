5aaa35535b0ae4c69e0b66cb73b068c7e8f0f1a1
Muhammad Sulaiman <mksuleiman03@gmail.com>
2020-06-24 21:09:13 +0100

    added orderable

--- home/models.py
+++ home/models.py
@@ -1,12 +1,31 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, PageChooserPanel, InlinePanel, MultiFieldPanel
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the homepage carousel"""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        ImageChooserPanel("carousel_image")
+    ]
 
 
 class HomePage(Page):
@@ -18,6 +37,7 @@ class HomePage(Page):
     banner_title = models.CharField(max_length=100, blank=True, null=True)
     banner_subtitle = RichTextField(
         blank=True, null=True, features=["bold", "italic"])
+
     banner_image = models.ForeignKey(
         "wagtailimages.Image",
         null=True,
@@ -46,12 +66,21 @@ class HomePage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
-        FieldPanel('body', classname="full"),
+        MultiFieldPanel([
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image"),
+            PageChooserPanel("banner_cta"),
+        ], heading="Banner Options"),
+
+        MultiFieldPanel([
+            InlinePanel("carousel_images", max_num=5,
+                        min_num=1, label="Image"),
+        ], heading="carousel Images"),
+
         StreamFieldPanel("content"),
+        FieldPanel('body', classname="full"),
+
     ]
 
     class Meta:

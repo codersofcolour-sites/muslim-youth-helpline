699e6cb232768d07c8ee05325b0ff6c078a621e0
Muhammad Sulaiman <mksuleiman03@gmail.com>
2020-06-24 13:30:46 +0100

    added listblocks, created cards

--- flex/models.py
+++ flex/models.py
@@ -15,7 +15,10 @@ class FlexPage(Page):
 
     content = StreamField(
         [
-            ("title_and_text", blocks.TitleAndTextBlock())
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichtextBlock()),
+            ("simple_richtext", blocks.SimpleRichtextBlock()),
+            ("cards", blocks.CardBlock()),
         ],
         null=True,
         blank=True,

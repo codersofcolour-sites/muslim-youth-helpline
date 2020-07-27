70255b75f7907373aabc335f97b6898faaab81f4
Muhammad Sulaiman <mksuleiman03@gmail.com>
2020-07-27 21:50:43 +0100

    Revert "fixing errors"
    This reverts commit 94bf700067d80386fc65ac7bc4f6d69f8adb650f.

--- flex/models.py
+++ flex/models.py
@@ -19,7 +19,7 @@ class FlexPage(Page):
             ("full_richtext", blocks.RichtextBlock()),
             ("simple_richtext", blocks.SimpleRichtextBlock()),
             ("cards", blocks.CardBlock()),
-            ("cta", blocks.CTABlock()),
+            ("CTA", blocks.CTABlock()),
             ("button", blocks.ButtonBlock()),
             ("HTML", blocks.HTMLBlock()),
         ],
